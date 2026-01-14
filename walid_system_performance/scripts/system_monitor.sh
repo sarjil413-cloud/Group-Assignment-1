8f75ed53c7db021012f281234b8386b94165e09f
mahmoudwalid4321-tech <mahmoudwalid4321@gmail.com>
2026-01-14 14:46:34 +0800

    Fixed paths and improved system monitoring script

--- walid_system_performance/scripts/system_monitor.sh
+++ walid_system_performance/scripts/system_monitor.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-date >> system_logs/system_report.txt
-uptime >> system_logs/system_report.txt
-free -h >> system_logs/system_report.txt
-df -h / >> system_logs/system_report.txt
-ps aux --sort=-%cpu | head -4 >> system_logs/system_report.txt
-ps aux --sort=-%mem | head -4 >> system_logs/system_report.txt
-echo "----------------" >> system_logs/system_report.txt
+date >> ~/Group-Assignment-1/walid_system_performance/system_logs/system_report.txt
+uptime >>~/Group-Assignment-1/walid_system_performance/system_logs/system_report.txt
+free -h >> ~/Group-Assignment-1/walid_system_performance/system_logs/system_report.txt
+df -h / >> ~/Group-Assignment-1/walid_system_performance/system_logs/system_report.txt
+ps aux --sort=-%cpu | head -4 >>~/Group-Assignment-1/walid_system_performance/system_logs/system_report.txt
+ps aux --sort=-%mem | head -4 >> ~/Group-Assignment-1/walid_system_performance/system_logs/system_report.txt
+echo "----------------" >> ~/Group-Assignment-1/walid_system_performance/system_logs/system_report.txt
