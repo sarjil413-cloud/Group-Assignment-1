16d243f246f0c892397fad287d1d04a61bffa5b4
mahmoudwalid4321-tech <mahmoudwalid4321@gmail.com>
2026-01-29 17:58:38 +0800

    Add comments explaining system monitoring commands

--- walid_system_performance/scripts/system_monitor.sh
+++ walid_system_performance/scripts/system_monitor.sh
@@ -6,3 +6,5 @@ df -h / >> ~/Group-Assignment-1/walid_system_performance/system_logs/system_repo
 ps aux --sort=-%cpu | head -4 >>~/Group-Assignment-1/walid_system_performance/system_logs/system_report.txt
 ps aux --sort=-%mem | head -4 >> ~/Group-Assignment-1/walid_system_performance/system_logs/system_report.txt
 echo "----------------" >> ~/Group-Assignment-1/walid_system_performance/system_logs/system_report.txt
+# This command collects memory usage
+# This command logs disk usage
