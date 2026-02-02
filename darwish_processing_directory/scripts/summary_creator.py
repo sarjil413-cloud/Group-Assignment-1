888b8df6df20442b50658722ec564e14e993d28a
Darwish-mmu <MUHAMMAD.DARWISH.AHMAD@student.mmu.edu.my>
2026-02-03 05:21:01 +0800

    Added system report reading

--- darwish_processing_directory/scripts/summary_creator.py
+++ darwish_processing_directory/scripts/summary_creator.py
@@ -2,19 +2,27 @@ from datetime import date, datetime
 from collections import Counter
 from pathlib import Path
 
-# SUMMARY FILE
+# FILE
 current_pwd = Path(__file__).resolve()
+parent_folder = current_pwd.parent.parent
+repos = parent_folder.parent
 
 # SUMMARY FILE PATHS
-summary_pwd = current_pwd.parent.parent / "summaries"
+summary_pwd = parent_folder / "summaries"
 
 # EVENT FILE PATHS
 monitor_log_name = 'events.log'
-monitor_pwd = current_pwd.parent.parent.parent / "sarjil_directory_monitoring/logs"
+monitor_pwd = repos / "sarjil_directory_monitoring/logs"
 events_log_file = monitor_pwd / monitor_log_name
 
 total_events = []
 created_bytes = 0
+
+# SYSTEM REPORTS
+
+system_log_name = 'system_report.txt'
+system_log_pwd = repos / "walid_system_performance/system_logs"
+system_report_file = system_log_pwd / system_load_name
 
 # GET EVENTS
 
@@ -47,7 +55,7 @@ print(created_bytes)
 
 current_date = date.today()
 current_timelog = datetime.now()
-summary_file_name = "summary" + str(current_date) + ".txt"
+summary_file_name = "summary_" + str(current_date) + ".txt"
 with open(summary_pwd / summary_file_name, "w") as f:
 	
 	log = str(current_timelog) + "\n" + "=" * 25 # Get the current time
@@ -56,3 +64,5 @@ with open(summary_pwd / summary_file_name, "w") as f:
 	log += f"\nFiles deleted: {event_counter['DELETED']}"
 
 	f.write(log)
+
+print("Summary successfully created")
